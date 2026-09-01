189ffd0a75c473a3ef026d21695f78009e870fa3
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-01 13:39:21 -0300

    Add createProject function to manage project creation

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,1 +1,33 @@
+import { STATUS } from "../data/constants";
+import { todayISO } from "../utils/dateUtils";
 
+function uid() {
+  return Math.random()
+    .toString(36)
+    .slice(2, 10);
+}
+
+function createProject({
+  name,
+  responsavel,
+  startDate,
+  currentUser,
+}) {
+  return {
+    id: uid(),
+    name,
+    responsavel,
+    startDate,
+    createdAt: todayISO(),
+    updatedAt: todayISO(),
+    createdBy: currentUser.id,
+    currentStep: 0,
+    status: STATUS.EM_ANDAMENTO,
+    emailNotified: false,
+    data: {},
+  };
+}
+
+export{ 
+  createProject,
+}
