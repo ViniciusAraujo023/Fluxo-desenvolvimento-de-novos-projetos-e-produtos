6fe369f04cc7b9f5ee7900bffeb49c1858f692b6
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-01 11:25:11 -0300

    Update fileUtils.js

--- src/utils/fileUtils.js
+++ src/utils/fileUtils.js
@@ -1,1 +1,18 @@
+function readFileAsDataURL(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = () => reject(reader.error);
+    reader.readAsDataURL(file);
+  });
+}
 
+function downloadAttachedFile(value) {
+  if (!value?.url) return;
+  const a = document.createElement("a");
+  a.href = value.url;
+  a.download = value.name || "arquivo";
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+}
