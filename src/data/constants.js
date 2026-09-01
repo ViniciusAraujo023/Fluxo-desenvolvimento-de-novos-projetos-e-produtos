2c35e232095c540da587a7de66105c8c6aff76f9
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-01 11:18:29 -0300

    Add constants for status and options

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,1 +1,16 @@
+export const STATUS = {
+  EM_ANDAMENTO: "Em andamento",
+  CONCLUIDO: "Concluído",
+  RECUSADO: "Recusado",
+  CANCELADO: "Cancelado",
+};
 
+export const OPT_SN = [
+  "Sim",
+  "Não"
+];
+
+export const OPT_AR = [
+  "Aprovado",
+  "Reprovado"
+];
