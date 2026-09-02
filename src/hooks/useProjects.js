bf912fa235de7fd0ca4d5e0237198126f2113187
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-02 07:58:57 -0300

    Implement useProjects hook to load projects

--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -1,1 +1,20 @@
+import { useState, useEffect } from "react";
 
+import {
+  loadProjects,
+} from "../services/supabase";
+
+export function useProjects() {
+  const [projects, setProjects] = useState(null);
+
+  useEffect(() => {
+    loadProjects().then((p) =>
+      setProjects(p)
+    );
+  }, []);
+
+  return {
+    projects,
+    setProjects,
+  };
+}
