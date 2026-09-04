81490177d7d3bc017cb23428461aab7eef185665
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-04 11:57:57 -0300

    Update ProtectedRoute.jsx

--- src/Auth/ProtectedRoute.jsx
+++ src/Auth/ProtectedRoute.jsx
@@ -1,1 +1,20 @@
+import { LoginPage } from "./LoginPage";
 
+
+function ProtectedRoute({ user, loading, children }) {
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        Carregando...
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <LoginPage />;
+  }
+
+  return children;
+}
+
+export { ProtectedRoute };
