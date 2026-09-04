131c251c8dad95b0693791cf7153696652f9d9d0
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-04 09:45:08 -0300

    Add role utility functions for user role checks

--- src/utils/roleUtils.js
+++ src/utils/roleUtils.js
@@ -1,1 +1,26 @@
+import {
+  ROLES,
+} from "../data/roles";
 
+function isAdmin(user) {
+  return user?.perfil === ROLES.ADMIN;
+}
+
+function isGestor(user) {
+  return user?.perfil === ROLES.GESTOR;
+}
+
+function isColaborador(user) {
+  return user?.perfil === ROLES.COLABORADOR;
+}
+
+function isVisualizador(user) {
+  return user?.perfil === ROLES.VISUALIZADOR;
+}
+
+export {
+  isAdmin,
+  isGestor,
+  isColaborador,
+  isVisualizador,
+};
