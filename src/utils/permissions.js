15c37f811827a27cda3305a4485fa4d6010ff76c
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-04 09:45:36 -0300

    Add permission utility functions for user roles

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -1,1 +1,38 @@
+import {
+  isAdmin,
+  isGestor,
+} from "./roleUtils";
 
+function canApprove(user) {
+  return (
+    isAdmin(user) ||
+    isGestor(user)
+  );
+}
+
+function canManageUsers(user) {
+  return isAdmin(user);
+}
+
+function canManageAreas(user) {
+  return isAdmin(user);
+}
+
+function canManageActivities(user) {
+  return isAdmin(user);
+}
+
+function canCreateProjects(user) {
+  return (
+    isAdmin(user) ||
+    isGestor(user)
+  );
+}
+
+export {
+  canApprove,
+  canManageUsers,
+  canManageAreas,
+  canManageActivities,
+  canCreateProjects,
+};
