900bbf4e93f2c691221c6303e7b466ab98584a53
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-08 13:42:03 -0300

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,1 +1,42 @@
+import { supabase } from "./authService";
 
+
+async function getUserByEmail(email) {
+  const { data, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("email", email)
+    .single();
+
+  if (error) {
+    return null;
+  }
+
+  return data;
+}
+
+async function createUser({
+  email,
+  nome,
+}) {
+  const { data, error } = await supabase
+    .from("users")
+    .insert([
+      {
+        email,
+        nome,
+        perfil: "visualizador",
+        aprovado: false,
+      },
+    ])
+    .select()
+    .single();
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+}
+
+export { getUserByEmail, createUser, };
