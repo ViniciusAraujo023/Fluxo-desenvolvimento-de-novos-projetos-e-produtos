39f0a6e238e7131148278dbaf348dde98c7615a6
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-08 13:46:18 -0300

    Update and rename VerifyAcess to VerifyAcess.jsx

---
+++ src/Auth/VerifyAcess.jsx
@@ -0,0 +1,10 @@
+function VerifyAccess() {
+  return (
+    <div>
+      <h1>Aguardando aprovação</h1>
+      <p>Sua conta precisa ser aprovada por um administrador.</p>
+    </div>
+  );
+}
+
+export { VerifyAccess };
