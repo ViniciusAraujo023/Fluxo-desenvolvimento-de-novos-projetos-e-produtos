9aed1121ced4d39a0d3c55f1e80c49274d505aca
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-09 09:19:36 -0300

    Fix import path for SettingsPage component

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import { NewProjectForm, } from "./components/Dashboard/NewProjectForm";
 import { LoadingScreen, } from "./components/Layout/LoadingScreen";
 import { useAuth } from "./hooks/useAuth";
 import { ProtectedRoute } from "./Auth/ProtectedRoute";
-import { SettingsPage } from "./components/settings/SettingsPage";
+import { SettingsPage } from "./settings/SettingsPage";
 
 ///
 function App() {
