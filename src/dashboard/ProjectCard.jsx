b226819634c81b62be9503800c2a5385f9ad6bd9
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-14 09:18:12 -0300

    Update import paths in ProjectCard component

--- src/dashboard/ProjectCard.jsx
+++ src/dashboard/ProjectCard.jsx
@@ -1,8 +1,8 @@
 import { Calendar, User, RotateCcw, } from "lucide-react";
-import { fmtDate, } from "../../utils/dateUtils";
-import { statusBadgeClass, progressBarClass, isBlockedStatus, } from "../../utils/statusUtils";
-import { phaseOf, } from "../../data/phases";
-import { STEP_DEFS, } from "../../data/stepDefs";
+import { fmtDate, } from "./utils/dateUtils";
+import { statusBadgeClass, progressBarClass, isBlockedStatus, } from "./utils/statusUtils";
+import { phaseOf, } from "./data/phases";
+import { STEP_DEFS, } from "./data/stepDefs";
 
 function ProjectCard({ project, onOpen, onReactivate, isAdmin, }) 
 {
