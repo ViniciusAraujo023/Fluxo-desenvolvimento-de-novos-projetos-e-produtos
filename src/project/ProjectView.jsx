ecf545c12bdc4f84d780dc6224bcb0a9a79bb60c
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-14 09:16:10 -0300

    Refactor import paths in ProjectView.jsx

--- src/project/ProjectView.jsx
+++ src/project/ProjectView.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 import { ArrowLeft, Calendar, User, Check, Circle, ChevronLeft, AlertCircle, RotateCcw, ShieldCheck, Mail, Send, Ban, Trash2, } from "lucide-react";
 import { Field } from "./Field";
-import { STEP_DEFS } from "../../data/stepDefs";
-import { PHASES, phaseOf, } from "../../data/phases";
-import { STATUS, } from "../../data/constants";
-import { todayISO, fmtDate, } from "../../utils/dateUtils";
-import { isBlockedStatus, statusBadgeClass, } from "../../utils/statusUtils";
-import { notifyNewIdea, buildIdeaMailto, EMAILJS_READY, NOTIFY_EMAIL, } from "../../services/emailService";
-import { SopranoMark, } from "../Layout/SopranoMark";
-import { ProcessingOverlay, } from "../Layout/ProcessingOverlay";
+import { STEP_DEFS } from "./data/stepDefs";
+import { PHASES, phaseOf, } from "./data/phases";
+import { STATUS, } from "./data/constants";
+import { todayISO, fmtDate, } from "./utils/dateUtils";
+import { isBlockedStatus, statusBadgeClass, } from "./utils/statusUtils";
+import { notifyNewIdea, buildIdeaMailto, EMAILJS_READY, NOTIFY_EMAIL, } from "./services/emailService";
+import { SopranoMark, } from "./components/Layout/SopranoMark";
+import { ProcessingOverlay, } from "./components/Layout/ProcessingOverlay";
 
 const fieldsFor = (idx, project) => {
   const def = STEP_DEFS[idx];
