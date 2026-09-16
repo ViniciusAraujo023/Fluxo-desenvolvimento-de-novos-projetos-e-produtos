1d653cf1b8aecc0c04f8958d643ffd70eb38482f
Vinícius Araújo <103593308+ViniciusAraujo023@users.noreply.github.com>
2026-09-16 10:29:33 -0300

    Update useProjects.js

--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
-
-import {
-  loadProjects,
-} from "../services/supabase";
+import { loadProjects, } from "../services/supabase";
 
 export function useProjects() {
   const [projects, setProjects] = useState(null);
